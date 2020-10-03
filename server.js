e35d8cf32dfbd8b430cb3078b6f7c0aba7d32851
Andrew0502 <agcmarcus@gmail.com>
2020-10-03 16:26:26 -0400

    corrected html paths by adding __dirname. added app.listen. skeletonized the get post and delete functions.

--- server.js
+++ server.js
@@ -9,9 +9,35 @@ app.use(express.static(path.join("./Develop/public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+
 app.get("/", function(req, res){
-    res.sendFile(path.join("./Develop/public/index.html"));
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
 });
 app.get("/notes", function(req, res){
-    res.sendFile(path.join("./Develop/public/notes.html"));
+    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+});
+
+
+
+app.get("/api/notes", function(req, res){
+    fs.readFile("./Develop/db/db.json", function(err, data){
+
+    });
+});
+
+app.post("/api/notes", function(req, res){
+    fs.readFile("./Develop/db/db.json", "utf-8", function(err, data){
+    
+    });   
+});
+
+app.delete("/api/notes/:id", function(req,res){
+    fs.readFile("./Develop/db/db.json", "utf-8", (err, data) => {
+        
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`App is running on http://localhost:${PORT}`);
 });

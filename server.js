6d5b002adbb388974f9c06a7bbda94ae71773cf9
Andrew0502 <agcmarcus@gmail.com>
2020-10-03 15:48:21 -0400

    Added Middleware and html paths.

--- server.js
+++ server.js
@@ -4,3 +4,14 @@ const fs = require("fs");
 
 const app = express(); 
 const PORT = process.env.PORT || 8080; 
+
+app.use(express.static(path.join("./Develop/public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function(req, res){
+    res.sendFile(path.join("./Develop/public/index.html"));
+});
+app.get("/notes", function(req, res){
+    res.sendFile(path.join("./Develop/public/notes.html"));
+});
